7f67cf9f8eecd724c9bd8a753432e0d016e9611c
Garry Ledwith <garryledwith@gmail.com>
2025-10-12 07:38:35 +0100

    added comments for user endpoints to prepare for setting up routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,3 +1,18 @@
+/*
+Users endpoints:
+
+POST /api/v1/auth/sign-up (creates a user account)  
+
+POST /api/v1/auth/login (login to existing account)  
+
+GET /api/v1/users (admin) (lists all the users)  
+
+PUT /api/v1/users/:id (admin: update user ) 
+
+DELETE /api/v1/users/:id (admin: deletes a user account)  
+
+*/
+
 import express, {Router} from 'express';
 
 import {
