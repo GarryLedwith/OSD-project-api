f1986e8fbd2e318ab8f8f9d26b80a3d867d05ffa
Garry Ledwith <garryledwith@gmail.com>
2025-10-17 17:19:37 +0100

    updated equipment to include bookings

--- src/controllers/equipment.ts
+++ src/controllers/equipment.ts
@@ -21,6 +21,27 @@ Partial update of an item (change location or update status of item)
 
 DELETE /api/v1/equipment/:id (admin: delete an equipment item ) 
 
+
+Bookings sub-routes (embedded)  
+
+POST /api/v1/equipment/:id/bookings  (student: request loan item for a data range) 
+
+GET /api/v1/equipment/:id/bookings (any: own bookings for student; all bookings for staff/admin; filter by status) 
+
+Example query: status=pending | approved | denied |checked_out| returned 
+
+PATCH /api/v1/equipment/:id/bookings/:bookingId/approve (staff: approve a pending booking) 
+
+PATCH /api/v1/equipment/:id/bookings/:bookingId/deny (staff: deny a pending booking) 
+
+PATCH /api/v1/equipment/:id/bookings/:bookingId/check-out (staff: mark an item as checked out) 
+
+When item leaves inventory 
+
+PATCH /api/v1/equipment/:id/bookings/:bookingId/check-in (staff: mark an item as checked in) 
+
+Item is returned to inventory 
+
 */
 
 // add new equipment to the database
