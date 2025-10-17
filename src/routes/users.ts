f2ef80a4dadfe8ea7326b058d2e66f2062e0b7b5
Garry Ledwith <garryledwith@gmail.com>
2025-10-17 17:16:14 +0100

    added validation to PUT Request route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,18 +1,3 @@
-/*
-Users endpoints:
-
-GET /api/v1/users (admin: lists all the users)  
-
-GET /api/v1/users/:id (admin: gets an individual user)  
-
-PUT /api/v1/users/:id (admin: update individual user- full update ) 
-
-PATCH /api/v1/users/:id (admin: update individual user – partial update) 
-
-DELETE /api/v1/users/:id (admin: deletes a user account)  
-
-*/
-
 import express, {Router} from 'express';
 import { createUserSchema } from '../models/user';
 import { validate } from '../middleware/validate.middleware';               
@@ -24,15 +9,36 @@ import {
  updateUser,
  patchUser,
  deleteUser,
+ getUsersByRole,
 } from '../controllers/users';  
+
+/*
+Users endpoints:
+
+POST /api/v1/users (admin: creates a new user account)
+
+GET /api/v1/users (admin: lists all the users)  
+
+GET /api/v1/users/:id (admin: gets an individual user)  
+
+GET /api/v1/users/role/:role (admin: gets users by role)
+
+PUT /api/v1/users/:id (admin: update individual user- full update ) 
+
+PATCH /api/v1/users/:id (admin: update individual user – partial update) 
+
+DELETE /api/v1/users/:id (admin: deletes a user account)   
+
+*/
 
 const router: Router = express.Router();
 
 // Users routes - CRUD operations
 router.get('/', getUsers);
 router.get('/:id', getUserById);
+router.get('/role/:role', getUsersByRole);
 router.post('/', validate(createUserSchema), createUser);
-router.put('/:id', updateUser);
+router.put('/:id', validate(createUserSchema), updateUser);
 router.patch('/:id', patchUser);
 router.delete('/:id', deleteUser);
 
