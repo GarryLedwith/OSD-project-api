a35d3f281e90e062b227ee9ec6a7faf30bf43b59
Garry Ledwith <garryledwith@gmail.com>
2025-10-17 17:20:08 +0100

    updated equipment routes

--- src/routes/equipment.ts
+++ src/routes/equipment.ts
@@ -15,6 +15,27 @@ Partial update of an item (change location or update status of item)
 
 DELETE /api/v1/equipment/:id (admin: delete an equipment item ) 
 
+
+Bookings sub-routes (embedded)  
+
+POST /api/v1/equipment/:id/bookings  (student: request loan item for a data range) 
+
+GET /api/v1/equipment/:id/bookings (any: own bookings for student; all bookings for staff/admin; filter by status) 
+
+Example query: status=pending | approved | denied |checked_out| returned 
+
+PATCH /api/v1/equipment/:id/bookings/:bookingId/approve (staff: approve a pending booking) 
+
+PATCH /api/v1/equipment/:id/bookings/:bookingId/deny (staff: deny a pending booking) 
+
+PATCH /api/v1/equipment/:id/bookings/:bookingId/check-out (staff: mark an item as checked out) 
+
+When item leaves inventory 
+
+PATCH /api/v1/equipment/:id/bookings/:bookingId/check-in (staff: mark an item as checked in) 
+
+Item is returned to inventory 
+
 */
 import express, { Router } from "express";
 import { addEquipment } from "../controllers/equipment";
