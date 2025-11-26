94ebef4947a99c9ec57a2d0b2d99ac80a70202e0
Garry Ledwith <garryledwith@gmail.com>
2025-11-26 06:16:47 +0000

    validated patch route and added comments for clarity

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,5 @@
 import express, {Router} from 'express';
-import { createUserSchema } from '../models/user';
+import { createUserSchema, updateUserSchema } from '../models/user';
 import { validate } from '../middleware/validate.middleware';               
 
 import {
@@ -12,34 +12,15 @@ import {
  getUsersByRole,
 } from '../controllers/users';  
 
-/*
-Users endpoints:
-
-POST /api/v1/users (admin: creates a new user account)
-
-GET /api/v1/users (admin: lists all the users)  
-
-GET /api/v1/users/:id (admin: gets an individual user)  
-
-GET /api/v1/users/role/:role (admin: gets users by role)
-
-PUT /api/v1/users/:id (admin: update individual user- full update ) 
-
-PATCH /api/v1/users/:id (admin: update individual user – partial update) 
-
-DELETE /api/v1/users/:id (admin: deletes a user account)   
-
-*/
-
 const router: Router = express.Router();
 
 // Users routes - CRUD operations
 router.get('/', getUsers);
+router.get('/role/:role', getUsersByRole); 
 router.get('/:id', getUserById);
-router.get('/role/:role', getUsersByRole);
-router.post('/', validate(createUserSchema), createUser);
-router.put('/:id', validate(createUserSchema), updateUser);
-router.patch('/:id', patchUser);
+router.post('/', validate(createUserSchema), createUser); // validate request body
+router.put('/:id', validate(createUserSchema), updateUser); // validate request body
+router.patch('/:id', validate(updateUserSchema), patchUser); // validate request body
 router.delete('/:id', deleteUser);
 
 export default router;
