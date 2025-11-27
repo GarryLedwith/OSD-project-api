5f4d716f220aaf56c2364b532d5ac6d19f44ddf1
Garry Ledwith <garryledwith@gmail.com>
2025-11-27 21:39:51 +0000

    added cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, {Application, Request, Response} from "express" ;
 import userRoutes from './routes/users';
+import equipmentRoutes from './routes/equipment';   
 import morgan from "morgan";
+import cors from "cors";
 import dotenv from 'dotenv';
 import { initDb } from "./database";
+
 
 dotenv.config();
 
@@ -14,12 +17,15 @@ initDb(); // Initialize database connection
 
 app.use(morgan("tiny")); 
 
+app.use(cors()); // CORS middleware
+
 app.use(express.json());
-
-
 
 // register routes for users 
 app.use('/api/v1/users', userRoutes)
+
+// register routes for equipment
+app.use('/api/v1/equipment', equipmentRoutes);
 
 app.get("/health", async (_req: Request, res: Response) => {
     res.json({
