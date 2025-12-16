118482f5d0f7060e7efd34f030cee01be174ac4d
Garry Ledwith <garryledwith@gmail.com>
2025-12-16 16:12:11 +0000

    debugging auth route for login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { handleLogin } from '../controllers/auth';
+import { isAdmin } from '../middleware/auth.middleware';
 
 export const authRouter = express.Router();
 
 // public route for user login
 authRouter.post('/', handleLogin);
+
+
